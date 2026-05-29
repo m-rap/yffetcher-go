4e7491d6905a00e74ed9a6085fab659c046571a3
rian <mohammad.rian.ap@gmail.com>
2026-05-29 18:10:35 +0700

    fix fetch executable

--- tool/fetchtosqlite/main.go
+++ tool/fetchtosqlite/main.go
@@ -3,10 +3,30 @@ package main
 import (
 	"fmt"
 
-	"github.com/m-rap/yffetch-go"
+	yffetcher "github.com/m-rap/yffetch-go"
 )
 
 func main() {
 	fmt.Println("vim-go")
-	yffetch.Fetch()
+	searchItems := `[
+{"id":"AMRT","name":"AMRT",},
+{"id":"ARNA","name":"ARNA",},
+{"id":"BBCA","name":"BBCA",},
+{"id":"EKAD","name":"EKAD",},
+{"id":"GHON","name":"GHON",},
+{"id":"GOTO","name":"GOTO",},
+{"id":"INDF","name":"INDF",},
+{"id":"INDS","name":"INDS",},
+{"id":"INDY","name":"INDY",},
+{"id":"MPMX","name":"MPMX",},
+{"id":"OMED","name":"OMED",},
+{"id":"ULTJ","name":"ULTJ",},
+{"id":"XIHD","name":"XIHD",},
+{"id":"WSKT","name":"WSKT",},
+{"id":"MBMA","name":"MBMA",},
+]`
+	err := yffetcher.Fetch("itemprice.db", []byte(searchItems))
+	if err != nil {
+		fmt.Printf("error: %v\n", err)
+	}
 }
