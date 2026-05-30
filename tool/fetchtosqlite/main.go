ef98e73dcc2e67e862cb9c1c0f64e07e39272647
rian <mohammad.rian.ap@gmail.com>
2026-05-30 23:58:31 +0700

    fix fetchtosqlite

--- tool/fetchtosqlite/main.go
+++ tool/fetchtosqlite/main.go
@@ -9,23 +9,23 @@ import (
 func main() {
 	fmt.Println("vim-go")
 	searchItems := `[
-{"id":"AMRT","name":"AMRT",},
-{"id":"ARNA","name":"ARNA",},
-{"id":"BBCA","name":"BBCA",},
-{"id":"EKAD","name":"EKAD",},
-{"id":"GHON","name":"GHON",},
-{"id":"GOTO","name":"GOTO",},
-{"id":"INDF","name":"INDF",},
-{"id":"INDS","name":"INDS",},
-{"id":"INDY","name":"INDY",},
-{"id":"MPMX","name":"MPMX",},
-{"id":"OMED","name":"OMED",},
-{"id":"ULTJ","name":"ULTJ",},
-{"id":"XIHD","name":"XIHD",},
-{"id":"WSKT","name":"WSKT",},
-{"id":"MBMA","name":"MBMA",},
+	{"id":"AMRT","name":"AMRT",},
+	{"id":"ARNA","name":"ARNA",},
+	{"id":"BBCA","name":"BBCA",},
+	{"id":"EKAD","name":"EKAD",},
+	{"id":"GHON","name":"GHON",},
+	{"id":"GOTO","name":"GOTO",},
+	{"id":"INDF","name":"INDF",},
+	{"id":"INDS","name":"INDS",},
+	{"id":"INDY","name":"INDY",},
+	{"id":"MPMX","name":"MPMX",},
+	{"id":"OMED","name":"OMED",},
+	{"id":"ULTJ","name":"ULTJ",},
+	{"id":"XIHD","name":"XIHD",},
+	{"id":"WSKT","name":"WSKT",},
+	{"id":"MBMA","name":"MBMA",},
 ]`
-	err := yffetcher.Fetch("itemprice.db", []byte(searchItems))
+	err := yffetcher.FetchFromJson("itemprice.db", []byte(searchItems))
 	if err != nil {
 		fmt.Printf("error: %v\n", err)
 	}
